2e2efcd3d7bd03dd6393b2588ddb863ba1057364
rico sun <ricosr@163.com>
2018-11-23 18:43:40 +0800

    not all done, add config done

--- config.py
+++ config.py
@@ -6,8 +6,10 @@
 index_dict = {"AI": "index/AI"}
 file_dict = {"AI": "data/AI.pkl", "profile": "data/profile.pkl", "conversations": "data/conversations.pkl",
              "emotion": "data/emotion.pkl", "food": "data/food.pkl", "gossip": "data/gossip.pkl",
-             "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", "": "data/.pkl",
-             "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", "": "data/.pkl", }
+             "greetings": "data/greetings.pkl", "history": "data/history.pkl", "humor": "data/humor.pkl",
+             "literature": "data/literature.pkl", "money": "data/money.pkl", "movies": "data/movies.pkl",
+             "politics": "data/politics.pkl", "psychology": "data/psychology.pkl", "science": "data/science.pkl",
+             "sports": "data/sports.pkl", "trivia": "data/trivia.pkl", }
 model_dict = {"AI": "model/AI.pkl"}
 
 
